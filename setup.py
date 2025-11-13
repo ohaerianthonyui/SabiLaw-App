88ba8c04e2176d9bdb55b44414ed5b3ef9dafcb8
ohaerianthonyui <ohaerianthonyui@gmail.com>
2025-11-13 15:41:26 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from _setuptools import find_packages, setup
+
+setup(
+    name="SabiLaw",
+    version="0.1.0",
+    author="Anthony Ohaeri",
+    author_email ="ohaerianthonyui@gmail.com",
+    packages=find_packages(),
+    install_requeires=[]
+)
